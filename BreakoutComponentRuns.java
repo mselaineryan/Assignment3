5cc853266e10ccf5c0024d8bdbdd925c20e36b74
S <s>
2013-08-19 10:27:24 -1000

    {"type":"TI", "runs":"0"}

---
+++ BreakoutComponentRuns.java
@@ -0,0 +1,96 @@
+/*
+ * File: BreakoutComponentRuns.java
+ * -------------------
+ * Name:
+ * Section Leader:
+ * 
+ * This file is being used to test components (methods) of the Breakout game.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class BreakoutComponentRuns extends GraphicsProgram {
+
+/** Width and height of application window in pixels */
+	public static final int APPLICATION_WIDTH = 400;
+	public static final int APPLICATION_HEIGHT = 600;
+
+/** Dimensions of game board (usually the same) */
+	private static final int WIDTH = APPLICATION_WIDTH;
+	private static final int HEIGHT = APPLICATION_HEIGHT;
+
+/** Dimensions of the paddle */
+	private static final int PADDLE_WIDTH = 60;
+	private static final int PADDLE_HEIGHT = 10;
+
+/** Offset of the paddle up from the bottom */
+	private static final int PADDLE_Y_OFFSET = 30;
+
+/** Number of bricks per row */
+	private static final int NBRICKS_PER_ROW = 10;
+
+/** Number of rows of bricks */
+	private static final int NBRICK_ROWS = 10;
+
+/** Separation between bricks */
+	private static final int BRICK_SEP = 4;
+
+/** Width of a brick */
+	private static final int BRICK_WIDTH =
+	  (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+
+/** Height of a brick */
+	private static final int BRICK_HEIGHT = 8;
+
+/** Radius of the ball in pixels */
+	private static final int BALL_RADIUS = 10;
+
+/** Offset of the top brick row from the top */
+	private static final int BRICK_Y_OFFSET = 70;
+
+/** Number of turns */
+	private static final int NTURNS = 3;
+
+/* Method: run() */
+/** Runs the Breakout program. */
+	public void run() {
+		/* This method runs an outstanding and exciting version of
+		 * the arcade game Breakout! */
+		
+		/* top left corner of upper leftmost brick */
+		int startX = WIDTH - ((BRICK_WIDTH * NBRICKS_PER_ROW) + (BRICK_SEP * (NBRICKS_PER_ROW - 1))/2);
+		int startY = BRICK_Y_OFFSET;
+		
+		for (int i = 0; i < NBRICK_ROWS; i ++) {
+			
+			/* This loop lays down 10 bricks in a row, the outer loop lays down the columns.
+			 * The two loops could have been combined because the number rows and bricks per row 
+			 * are both 10, but they are separated so that the set up works even if one or both of
+			 * those parameters gets changed.
+			 */
+			for (int j = 0; j < NBRICKS_PER_ROW; j++) {
+			
+				int x = startX + ((BRICK_WIDTH + BRICK_SEP) * j);
+				int y = startY;
+				
+				GRect brick = new GRect (x, y, BRICK_WIDTH, BRICK_HEIGHT);
+			
+			}
+			
+			startY = startY + ((BRICK_HEIGHT + BRICK_SEP)* i);
+		
+	
+		}
+	
+	
+		
+	
+	
+
+}
