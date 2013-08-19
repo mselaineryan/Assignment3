fee859da8d92686a9b0d36b39f33efaa389085cc
S <s>
2013-08-19 10:55:58 -1000

    {"type":"TI", "runs":"1"}

--- BreakoutComponentRuns.java
+++ BreakoutComponentRuns.java
@@ -82,6 +82,10 @@ public class BreakoutComponentRuns extends GraphicsProgram {
 				
 				GRect brick = new GRect (x, y, BRICK_WIDTH, BRICK_HEIGHT);
 				
+				if (i == 0 && j == 0) {
+					brick.setFilled(true);
+					brick.setColor(Color.RED);
+				
 				
 				
 				
